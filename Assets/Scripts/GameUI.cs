2460a07288703444ae552a664a2407038f2bc91f
Max Stankevich <m.stankevicsh@gmail.com>
2016-01-30 14:03:27 +0200

    UPD: UI in main game scene

---
+++ Assets/Scripts/GameUI.cs
@@ -0,0 +1,20 @@
+﻿#region References
+using System.ComponentModel;
+using UnityEngine;
+using UnityEngine.UI;
+#endregion
+
+public class GameUI : MonoBehaviour
+{
+	public Slider fireLevelBar;
+
+	public void Start () { LevelManager.instance.PropertyChanged += OnLevelManagerUpdate; }
+
+	private void Repaint ()
+	{
+		var manager = LevelManager.instance;
+		fireLevelBar.normalizedValue = manager.Level;
+	}
+
+	private void OnLevelManagerUpdate (object sender, PropertyChangedEventArgs e) { Repaint(); }
+}
---
+++ Assets/Scripts/UIMenu.cs
@@ -0,0 +1,15 @@
+﻿#region References
+using UnityEngine;
+using UnityEngine.UI;
+#endregion
+
+public class UIMenu : MonoBehaviour
+{
+	public static UIMenu instance;
+
+	public Button btnPlay;
+
+	public void Awake () { instance = this; }
+
+	public void Start () { btnPlay.onClick.AddListener(() => { Debug.Log("PLAY"); }); }
+}
