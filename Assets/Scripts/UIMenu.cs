65cc38991b0688b1b08395ac7a5f357f2081ae3c
Max Stankevich <m.stankevicsh@gmail.com>
2016-01-31 04:40:34 +0200

    NEW: UI navigation, menus, tutorial stuff

--- Assets/Scripts/UIMenu.cs
+++ Assets/Scripts/UIMenu.cs
@@ -7,20 +7,50 @@ public class UIMenu : MonoBehaviour
 {
 	public static UIMenu instance;
 
-	public GameObject root;
 	public Button btnPlay;
+	public Button btnHowToPlay;
+	public Button btnMainMenu;
+
+	public GameObject rootLayout;
+	public GameObject gameOverLayout;
+	public GameObject howToPlayLayout;
+	public GameObject mainMenuLayout;
+	public GameObject toMainMenuLayout;
 
 	public void Awake () { instance = this; }
 
 	public void Start ()
 	{
 		GameUI.instance.root.SetActive(false);
-		btnPlay.onClick.AddListener(() =>
-		{
-			Debug.Log("PLAY");
-			root.SetActive(false);
-			GameUI.instance.root.SetActive(true);
-            LevelManager.instance.GameStarted = true;
-		});
+		btnPlay.onClick.AddListener(Play);
+		btnMainMenu.onClick.AddListener(() => Setlayout(LayoutType.MainMenu));
+		btnHowToPlay.onClick.AddListener(() => Setlayout(LayoutType.HowToPlay));
+	}
+
+	public void Setlayout (LayoutType layout)
+	{
+		rootLayout.SetActive(layout != LayoutType.Game);
+		gameOverLayout.SetActive(layout == LayoutType.GameOver);
+		mainMenuLayout.SetActive(layout == LayoutType.MainMenu);
+		howToPlayLayout.SetActive(layout == LayoutType.HowToPlay);
+		toMainMenuLayout.SetActive(layout != LayoutType.MainMenu);
+		GameUI.instance.root.SetActive(layout == LayoutType.Game);
+	}
+
+	public void Play ()
+	{
+		Debug.Log("PLAY");
+		Setlayout(LayoutType.Game);
+		LevelManager.instance.GameStarted = true;
+	}
+
+	public void GameOver () { Setlayout(LayoutType.GameOver); }
+
+	public enum LayoutType
+	{
+		MainMenu,
+		HowToPlay,
+		Game,
+		GameOver,
 	}
 }
