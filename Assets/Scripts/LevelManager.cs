b090ed000f1c7977bb10a4279a1d62cfae2515ed
Max Stankevich <m.stankevicsh@gmail.com>
2016-01-31 04:41:20 +0200

    DIRTY HACK: call gameover from LevelManeger

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -9,17 +9,24 @@ public class LevelManager : ReactiveObject
 	public static readonly LevelManager instance = new LevelManager();
 
 	[SerializeField] protected float level = 1f;
-    [SerializeField] protected float fireHp = 0.5f;
-    [SerializeField] protected int score = 0;
+	[SerializeField] protected float fireHp = 0.5f;
+	[SerializeField] protected int score = 0;
 	[SerializeField] protected int gameLevel = 1;
-    [SerializeField] protected int maxLevel = 10;
-    [SerializeField] protected bool gameStarted = false;
+	[SerializeField] protected int maxLevel = 10;
+	[SerializeField] protected bool gameStarted = false;
 
-    public float Level { get { return level; } set { SetProperty(ref level, value, "Level"); } }
-    public float FireHp { get { return fireHp; } set { SetProperty(ref fireHp, value, "FireHp"); } }
-    public int GameLevel { get { return gameLevel; } set { SetProperty(ref gameLevel, value, "GameLevel"); } }
+	public float Level { get { return level; } set { SetProperty(ref level, value, "Level"); } }
+	public float FireHp
+	{
+		get { return fireHp; }
+		set
+		{
+			if (!SetProperty(ref fireHp, value, "FireHp")) return;
+			if (fireHp <= 0f) UIMenu.instance.GameOver();
+		}
+	}
+	public int GameLevel { get { return gameLevel; } set { SetProperty(ref gameLevel, value, "GameLevel"); } }
 	public int Score { get { return score; } set { SetProperty(ref score, value, "Score"); } }
-    public int MaxLevel { get { return maxLevel; } set { SetProperty(ref maxLevel, value, "MaxLevel"); } }
-    public bool GameStarted { get { return gameStarted; } set { SetProperty(ref gameStarted, value, "GameStarted"); } }
-
+	public int MaxLevel { get { return maxLevel; } set { SetProperty(ref maxLevel, value, "MaxLevel"); } }
+	public bool GameStarted { get { return gameStarted; } set { SetProperty(ref gameStarted, value, "GameStarted"); } }
 }
