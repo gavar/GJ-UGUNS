7b7e0feb0505d468c641c97548a6cba7a19eb10d
Max Stankevich <m.stankevicsh@gmail.com>
2016-01-31 02:41:51 +0200

    NEW: some shitty title animation

---
+++ Assets/Scripts/UIDirtyVertex.cs
@@ -0,0 +1,16 @@
+﻿#region References
+using UnityEngine;
+using UnityEngine.UI;
+#endregion
+
+public class UIDirtyVertex : MonoBehaviour
+{
+	protected Graphic[] graphics;
+
+	public void Start () { graphics = GetComponentsInChildren<Graphic>(); }
+	public void LateUpdate ()
+	{
+		for (int i = 0, imax = graphics.Length; i < imax; i++)
+			graphics[i].SetVerticesDirty();
+	}
+}
