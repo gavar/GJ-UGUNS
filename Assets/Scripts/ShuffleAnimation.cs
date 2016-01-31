358edb3a9042615baa373d39fdcdf9293a88a4b7
Max Stankevich <m.stankevicsh@gmail.com>
2016-01-31 02:31:47 +0200

    NEW: people play random animation now

---
+++ Assets/Scripts/ShuffleAnimation.cs
@@ -0,0 +1,22 @@
+﻿#region References
+using UnityEngine;
+#endregion
+
+public class ShuffleAnimation : MonoBehaviour
+{
+	protected Animation animation;
+
+	public void Start () { animation = GetComponent<Animation>(); }
+	public void Update ()
+	{
+		if (animation == null) return;
+		if (animation.isPlaying) return;
+
+		var count = animation.GetClipCount();
+		var nextID = Random.Range(0, count);
+		var e = animation.GetEnumerator();
+		for (var i = 0; e.MoveNext() && i < nextID; i++) { }
+		var next = (e.Current as AnimationState);
+		animation.Play(next.name);
+	}
+}
